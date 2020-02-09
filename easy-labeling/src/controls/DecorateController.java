b195a65838a274bd880fd5c0c052b859a2c9ac5e
seongwon <seongwon@edu.hanbat.ac.kr>
2020-02-10 02:50:22 +0900

    InitController 첫 번째 페이지 구현 완료

---
+++ easy-labeling/src/controls/DecorateController.java
@@ -0,0 +1,54 @@
+package controls;
+
+import annotation.Component;
+import bind.DataBinding;
+import dao.*;
+import vo.Label;
+import vo.Paper;
+
+import java.util.Map;
+
+@Component("/decorate.do")
+public class DecorateController implements DataBinding, Controller {
+
+  PostgresSqlColorDao colorDao;
+  PostgresSqlWeightDao weightDao;
+  PostgresSqlFontSizeDao fontSizeDao;
+  PostgresSqlFontDao fontDao;
+
+  public void setColorDao(PostgresSqlColorDao colorDao) {
+    this.colorDao = colorDao;
+  }
+
+  public void setWeightDao(PostgresSqlWeightDao weightDao) {
+    this.weightDao = weightDao;
+  }
+
+  public void setFontSizeDao(PostgresSqlFontSizeDao fontSizeDao) {
+    this.fontSizeDao = fontSizeDao;
+  }
+
+  public void setFontDao(PostgresSqlFontDao fontDao) {
+    this.fontDao = fontDao;
+  }
+
+  @Override
+  public Object[] getDataBinders() {
+    return new Object[] {
+            "paper", Paper.class,
+            "label", Label.class
+    };
+  }
+
+  @Override
+  public String execute(Map<String, Object> model) throws Exception {
+    model.put("colors", colorDao.selectList());
+    model.put("weights", weightDao.selectList());
+    model.put("fontSizes", fontSizeDao.selectList());
+    model.put("fonts", fontDao.selectList());
+    model.put("label", model.get("label"));
+    // TODO 적용
+    return "/sample-web-page/DecoratePage.jsp";
+  }
+
+}
--- easy-labeling/src/controls/InitController.java
+++ easy-labeling/src/controls/InitController.java
@@ -2,20 +2,58 @@ package controls;
 
 import annotation.Component;
 import bind.DataBinding;
+import dao.PostgresSqlLabelDao;
+import dao.PostgresSqlPaperDao;
+import vo.Label;
+import vo.Paper;
 
+import javax.servlet.http.HttpSession;
 import java.util.Map;
 
-@Component("/init")
+@Component("/init.do")
 public class InitController implements DataBinding, Controller {
 
-  @Override
-  public Object[] getDataBinders() {
-    return new Object[0];
+  PostgresSqlPaperDao postgresSqlPaperDao;
+  PostgresSqlLabelDao postgresSqlLabelDao;
+
+  public void setPostgresSqlPaperDao(PostgresSqlPaperDao postgresSqlPaperDao) {
+    this.postgresSqlPaperDao = postgresSqlPaperDao;
+  }
+
+  public void setPostgresSqlLabelDao(PostgresSqlLabelDao postgresSqlLabelDao) {
+    this.postgresSqlLabelDao = postgresSqlLabelDao;
   }
 
   @Override
   public String execute(Map<String, Object> model) throws Exception {
-    return null;
+    Paper paper = (Paper)model.get("paper");
+    Label label = (Label)model.get("label");
+    if (paper.getHeight() == null) {
+      model.put("paperSizes", postgresSqlPaperDao.selectList());
+      model.put("labels", postgresSqlLabelDao.selectList());
+      return "/sample-web-page/InitPage.jsp";
+    } else {
+      HttpSession session = (HttpSession) model.get("session");
+      String width = paper.getWidth();
+      String height = paper.getHeight();
+      int intWidth = Integer.parseInt(width.substring(0, 3)) - 5;
+      String stringWidth = intWidth + "mm";
+      int intHeight = Integer.parseInt(height.substring(0, 3)) - 5;
+      String stringHeight = intHeight + "mm";
+      session.setAttribute("width", stringWidth);
+      session.setAttribute("height", stringHeight);
+      session.setAttribute("label", label);
+      return "redirect:/decorate.do";
+    }
+
+  }
+
+  @Override
+  public Object[] getDataBinders() {
+    return new Object[] {
+            "paper", Paper.class,
+            "label", Label.class
+    };
   }
 
 }
